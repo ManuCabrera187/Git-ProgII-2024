f2b27fd11a678423718ff2bb8709e832dbc18cf1
ManuCabrera187 <405281@tecnicatura.frc.utn.edu.ar>
2024-08-20 17:15:14 -0300

    Add files via upload

---
+++ InterfazPila/Clases/Cola.cs
@@ -0,0 +1,31 @@
+﻿using InterfazPila.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfazPila.Clases
+{
+    public class Cola : ColleccionAbstracta
+    {
+        public Cola(int size):base(size) 
+        {
+
+        }
+        public override object extraer()
+        {
+            object e = null;
+
+            if(!estaVacia())
+            {
+                int i;
+                for (i = 0; i < next && array[i] == null; i++) ;
+                e = array[i];
+                array[i] = null;
+            }
+
+            return e;
+        }
+    }
+}
---
+++ InterfazPila/Clases/Pila.cs
@@ -0,0 +1,35 @@
+﻿using InterfazPila.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfazPila.Clases
+{
+    public class Pila : ColleccionAbstracta
+    {
+        public Pila(int size):base(size) 
+        { 
+
+        }
+        public override object extraer()
+        {
+            object e = null;
+
+            if (!estaVacia())
+            {
+                //int i;
+                //for (i = 0; i < next && array[i] == null; i++) ;
+                //e = array[i];
+                //array[i] = null;
+
+                e = array[next];
+                array[next] = null;
+                next--;
+            }
+
+            return e;
+        }
+    }
+}

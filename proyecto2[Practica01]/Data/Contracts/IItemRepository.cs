ff2dc173ebb015c33be44f47762a481fddaa7428
ManuCabrera187 <405281@tecnicatura.frc.utn.edu.ar>
2024-09-10 11:02:23 -0300

    Add files via upload

---
+++ proyecto2[Practica01]/Data/Contracts/IItemRepository.cs
@@ -0,0 +1,17 @@
+﻿using proyecto2_Practica01_.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto2_Practica01_.Data.Contracts
+{
+    public interface IItemRepository
+    {
+        List<Item> GetAll();
+        Item GetById(int id);
+        bool Save(Item oItem);
+        bool Delete(int id);
+    }
+}
---
+++ proyecto2[Practica01]/Services/ItemManager.cs
@@ -0,0 +1,40 @@
+﻿using proyecto2_Practica01_.Data.Contracts;
+using proyecto2_Practica01_.Data.Implementations;
+using proyecto2_Practica01_.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto2_Practica01_.Services
+{
+    public class ItemManager
+    {
+        private IItemRepository _repositorio;
+
+        public ItemManager()
+        {
+            _repositorio = new ItemRepository();
+        }
+
+        public List<Item> GetItems()
+        {
+            return _repositorio.GetAll();
+        }
+
+        public Item GetItemById(int id)
+        {
+            return _repositorio.GetById(id);
+        }
+
+        public bool SaveItem(Item oItem)
+        {
+            return _repositorio.Save(oItem);
+        }
+        public bool DeleteItem(int id)
+        {
+            return _repositorio.Delete(id);
+        }
+    }
+}
